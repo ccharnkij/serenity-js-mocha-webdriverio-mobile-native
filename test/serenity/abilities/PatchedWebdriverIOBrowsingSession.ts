b36e78f0c53949101c95c85f77bf98a3ad47fb3f
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2024-10-14 23:02:09 +0100

    Experimenting with using context ID instead of the window handle

--- test/serenity/abilities/PatchedWebdriverIOBrowsingSession.ts
+++ test/serenity/abilities/PatchedWebdriverIOBrowsingSession.ts
@@ -1,6 +1,58 @@
 /* eslint-disable unicorn/filename-case */
-import { WebdriverIOBrowsingSession } from '@serenity-js/webdriverio';
+import { CorrelationId } from '@serenity-js/core/lib/model';
+import { WebdriverIOBrowsingSession, WebdriverIOPage } from '@serenity-js/webdriverio';
+import { WebdriverIOErrorHandler } from '@serenity-js/webdriverio/lib/screenplay/models/WebdriverIOErrorHandler';
+import {
+    WebdriverIOModalDialogHandler
+} from '@serenity-js/webdriverio/lib/screenplay/models/WebdriverIOModalDialogHandler';
 
 export class PatchedWebdriverIOBrowsingSession extends WebdriverIOBrowsingSession {
 
+    override async currentPage(): Promise<WebdriverIOPage> {
+        if (! this.browser.isMobile) {
+            return super.currentPage();
+        }
+
+        const actualCurrentPageId = await this.assignPageId();
+
+        if (this.currentBrowserPage && this.currentBrowserPage.id.equals(actualCurrentPageId)) {
+            return this.currentBrowserPage;
+        }
+
+        this.currentBrowserPage = await this.registerCurrentPage();
+
+        return this.currentBrowserPage;
+    }
+
+    protected override async registerCurrentPage(): Promise<WebdriverIOPage> {
+        const pageId = await this.assignPageId();
+
+        const errorHandler = new WebdriverIOErrorHandler();
+
+        const page = new WebdriverIOPage(
+            this,
+            this.browser,
+            new WebdriverIOModalDialogHandler(this.browser, errorHandler),
+            errorHandler,
+            pageId,
+        );
+
+        this.register(page)
+
+        return page;
+    }
+
+    private async assignPageId(): Promise<CorrelationId> {
+        const context = await this.browser.getContext();
+
+        if (typeof context === 'string') {
+            return new CorrelationId(context);
+        }
+
+        if (context.id) {
+            return new CorrelationId(context.id);
+        }
+
+        return CorrelationId.create();
+    }
 }
--- test/specs/app.spec.ts
+++ test/specs/app.spec.ts
@@ -3,11 +3,11 @@ import 'mocha'
 import { Ensure } from '@serenity-js/assertions'
 import { actorCalled, Duration } from '@serenity-js/core'
 import { By, Click, isVisible, PageElement } from '@serenity-js/web'
+import { BrowseTheWebWithWebdriverIO } from '@serenity-js/webdriverio';
 
+import { PatchedWebdriverIOBrowsingSession } from '../serenity/abilities/PatchedWebdriverIOBrowsingSession';
 import CartScreen from '../serenity/screens/CartScreen'
 import HomeScreen from '../serenity/screens/HomeScreen'
-import { BrowseTheWebWithWebdriverIO } from '@serenity-js/webdriverio';
-import { PatchedWebdriverIOBrowsingSession } from '../serenity/abilities/PatchedWebdriverIOBrowsingSession';
 
 describe('serenity-js Android & iOS app', () => {
 
@@ -20,7 +20,6 @@ describe('serenity-js Android & iOS app', () => {
      */
     it('uses WebdriverIO and should pass', async () => {
         await actorCalled('Alice')
-            .whoCan(new BrowseTheWebWithWebdriverIO(new PatchedWebdriverIOBrowsingSession(browser)))
             .attemptsTo(
                 HomeScreen.clicksFirstItem(),
                 CartScreen.expectGoShoppingButtonDisplayed()
@@ -32,15 +31,16 @@ describe('serenity-js Android & iOS app', () => {
      * does not exist for Appium
      */
     it(`uses SerenityJS and should fail due to function mapping error`, async () => {
-        await actorCalled('Alice').attemptsTo(
-            Click.on(browser.isAndroid ?
-                PageElement.located(By.css('~Displays number of items in your cart')) :
-                PageElement.located(By.css('~Cart-tab-item'))),
-            Ensure.eventually(browser.isAndroid ?
-                PageElement.located(By.css('android=new UiSelector().text("Go Shopping")')) :
-                PageElement.located(By.css('~GoShopping')), isVisible())
-            .timeoutAfter(Duration.ofSeconds(5))
-        )
-
+        await actorCalled('Alice')
+            .whoCan(new BrowseTheWebWithWebdriverIO(new PatchedWebdriverIOBrowsingSession(browser)))
+            .attemptsTo(
+                Click.on(browser.isAndroid ?
+                    PageElement.located(By.css('~Displays number of items in your cart')) :
+                    PageElement.located(By.css('~Cart-tab-item'))),
+                Ensure.eventually(browser.isAndroid ?
+                    PageElement.located(By.css('android=new UiSelector().text("Go Shopping")')) :
+                    PageElement.located(By.css('~GoShopping')), isVisible())
+                .timeoutAfter(Duration.ofSeconds(5))
+            )
     })
 })
